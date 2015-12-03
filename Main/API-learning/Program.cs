971bff3ff42bb22f48a5db546f996ac759f74e59
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-12-03 17:59:09 +0300

    Test code improved

--- Main/API-learning/Program.cs
+++ Main/API-learning/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using CWDev.SLNTools.Core;
 
-namespace APIlearning
+class MainClass
 {
-	class MainClass
+	public static void Main(string[] args)
 	{
-		public static void Main (string[] args)
+		try
 		{
-			Console.WriteLine ("Hello World!");
+			if (args.Length < 2)
+			{
+				Console.WriteLine ("{0}", "usage: API-learning {filename} {guid}");
+				Environment.Exit(ReturnCodes.NO_ENOUGH_ARGUMENTS);
+			}
+			var filename = args [1];
+			var project_id = args [2];
+			var sol = SolutionFile.FromFile (filename);
+			sol.Projects.Remove (project_id);
+			sol.SaveAs (filename + ".new");
+			Environment.Exit(ReturnCodes.SUCCESS);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine (ex.ToString ());
+			Environment.Exit(ReturnCodes.EXCEPTION);
 		}
 	}
 }
